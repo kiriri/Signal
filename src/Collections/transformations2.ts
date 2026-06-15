d9b3e1405d08a2dcf225d076ad51a2d03dd4aa61
kiriri <3406036+kiriri@users.noreply.github.com>
2026-06-15 19:59:33 +0200

    reimplemented collections

--- src/Collections/transformations2.ts
+++
@@ -1,9 +0,0 @@
-import { NativeSignal } from "../Core/NativeSignal.js";
-import { BufferedSubscribable } from "../Sinks/BufferedSubscribable.js";
-import { I_NativeCollection, ReqColTypes } from "./Collection.js";
-import { Computed } from "../Core/Computed.js";
-import { EventRef, I_GettableSubscribable, I_Subscribable, LinkedList, StatefulSubscribable, Subscribable } from "../Core/Subscribable.js";
-
-
-
-// At its core we need to listen for events and react to them. All abstractions might be bloat here.
